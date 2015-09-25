eba0d4f115e3eff1117e7ea1f507d7a07accf6ec
alien-col <abe@duarte.co>
2015-09-25 15:53:17 -0500

    recommit to unstash

--- easywebcode/easywebcontent/parser.py
+++ easywebcode/easywebcontent/parser.py
@@ -1,1 +1,10 @@
 __author__ = 'abeduarterey'
+import urllib2
+
+
+class parser_wikihow():
+
+    @staticmethod
+    def parse_wikihow_data():
+        data = urllib2.urlopen("http://www.wikihow.com/api.php?action=query&list=search&srwhat=text&format=json&srprop=snippet%7Cwordcount%7Csectionsnippet&srsearch=watch%20movie%20online").read()
+        data = data.split('\n')
--- easywebcode/easywebcontent/views.py
+++ easywebcode/easywebcontent/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from parser import parser_wikihow
 # Create your views here.
 
 def index(request):
     if request.method == 'GET': # If the form is submitted
         search_query = request.GET.get('search_box', None)
-        # Do whatever you need with the word the user looked for
+        parser_wikihow.parse_wikihow_data()
+        # the following are lines for implementing parsing
     return render(request, 'index.html')
     # Your code
+
